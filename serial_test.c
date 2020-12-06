22bb6bab73ab40c2d7cfd64fb26877701b02e519
Xing Yin <yinxing1995@163.com>
2020-12-06 03:20:24 +0000

    serial works stably.

--- serial_test.c
+++ serial_test.c
@@ -2,18 +2,22 @@
 #include<fcntl.h>
 #include<unistd.h>
 #include<string.h>
-#include <termios.h>
+#include<termios.h>
+#include<stdlib.h>
 
-char *p = "echo 900 1\\";
+char *p;
 
 int main(void)
 {
     int fd = 0;
+    p = (char *)malloc(10);
+    sprintf(p, "%d %d\\", 400, 1);
     fd = open("/dev/serial0",O_RDWR|O_NOCTTY|O_NDELAY);
     write(fd, p, strlen(p));
-    usleep(10000);
-   //tcflush(fd, TCOFLUSH);
+    tcflush(fd, TCOFLUSH);
+    free(p);
     close(fd);
     return 0;
 }
+
 
